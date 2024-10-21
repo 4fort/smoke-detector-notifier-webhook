c8bc89a471a2223cc42a5ce37145e2e8e640b6da
4Fort <dexterfortbsilva@gmail.com>
2024-10-21 12:08:31 +0800

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,16 @@ import express, { Request, Response } from "express";
 import { formatDate, statusCodeDecorator } from "./lib/utils";
 
 const logger = express().use((req: Request, res: Response, next) => {
-  const { method, url } = req;
+  const { method, url, headers } = req;
   const timestamp = formatDate(new Date(new Date().toISOString()));
 
   res.on("finish", () => {
     const statusCode = res.statusCode;
     let { status, emoji } = statusCodeDecorator(statusCode);
 
-    console.log(`${emoji}[${timestamp}]: ${status} ${method} ${url}`);
+    console.log(
+      `${emoji}[${timestamp}]: ${status} ${method} ${url} - ${headers["user-agent"]}`
+    );
   });
 
   next();
